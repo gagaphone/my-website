5631c59ceaac772cb3d6ef3d3ee5b2345eec177d
gagaphone <ogalanets@gmail.com>
2026-04-12 21:09:01 +0200

    Delete js/loader.js

--- js/loader.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener("load", () => {
-  const loader = document.getElementById("loader");
-  loader.classList.add("hidden"); // fade out
-  setTimeout(() => loader.style.display = "none", 400); // remove from DOM
-});
