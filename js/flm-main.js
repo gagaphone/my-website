bd03e602eeae45f9ff20cc2c4f86ce0b8079ba30
gagaphone <ogalanets@gmail.com>
2026-04-12 21:08:42 +0200

    Delete js/flm-main.js

--- js/flm-main.js
+++
@@ -1,9 +0,0 @@
-// Toggle menu visibility
-
-
-const floatingMenu = document.getElementById("floatingMenu");
-const expandedMenu = document.getElementById("expandedMenu");
-
-floatingMenu.addEventListener("click", () => {
-  expandedMenu.classList.toggle("show");
-});
